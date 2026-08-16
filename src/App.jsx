6dbba0255a683b9e47b3292295fd5e41b1d590d4
Malshanshanuka <malshanshanuka1216@gmail.com>
2026-08-16 19:29:33 +0530

    Add Register page connected to backend API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <h1 className="text-white p-8">Dashboard - Coming Soon</h1>
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
